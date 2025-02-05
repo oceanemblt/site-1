3d81022fad7e092fa35f8b2163b531537eaa73f4
Gregoire <graturat@systra.com>
2025-02-05 13:48:36 +0100

    explication erreur

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-
-const App = createApp(App);
-App.use(router);
-App.mount('#app');
-
-
-
-
+// tu import ton fichier App.vue que tu as appelé App
+// sauf que tu créais une constante App qui porte le même nom
+// const App = createApp(App);
+// App.use(router);
+// App.mount('#app');
+// ==>>
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+// ou tu peux nommer app comme tu le souhaite tant que c'est différent des noms des imports que tu utilises
